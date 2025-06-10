55d3ad4499639d4e5883c321f123f5b5691e00ec
abirge12 <142052808+abirge12@users.noreply.github.com>
2025-06-10 17:32:09 -0400

    update SVM modeling

--- SVM.py
+++ SVM.py
@@ -1,12 +1,13 @@
 """ SVM """
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 import pandas as pd
 from preprocessing import transform_data, preprocess_data
+from sklearn.metrics import accuracy_score
+
 
 
 df_clean = transform_data('songs.csv', 'genre')
-
 
 features = ['duration_ms', 'popularity', 'danceability', 'energy', 'loudness', 
            'speechiness', 'acousticness', 'instrumentalness', 'liveness', 
@@ -17,3 +18,15 @@ y = df_clean['genre']
 
 X_train, X_test, y_train, y_test = preprocess_data(X, y)
 
+model = SVC(
+    class_weight='balanced',
+    random_state=42,) # any other parameter necesssary?
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
+
+#feature importance? confusion matrix?
