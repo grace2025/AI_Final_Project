b5320b043a1109ece1fdfd9405ffa9a29a952dba
abirge12 <142052808+abirge12@users.noreply.github.com>
2025-06-10 16:46:06 -0400

    update preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,7 @@
 import pandas as pd
-import csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 
 df = pd.read_csv('songs.csv')
 
@@ -11,8 +13,40 @@ df_new = df_new.reset_index(drop=True)
 
 df_new['explicit'] = df_new['explicit'].astype(int)
 
+df_clean = df_new[df_new['genre'] != 'set()'].copy()
 
-df_new.to_csv('songs_clean.csv')
+# if value count is 1 or less, remove
+unqiue_genres = df_clean['genre'].value_counts()
+model_genres = unqiue_genres[unqiue_genres >= 2].index
 
-print(df_new['genre'].value_counts())
+df_clean = df_clean[df_clean['genre'].isin(model_genres)]
+
+df_clean.to_csv('songs_clean.csv')
+
+# print(df_clean['genre'].value_counts())
+
+
+features = ['duration_ms', 'popularity', 'danceability', 'energy', 'loudness', 
+           'speechiness', 'acousticness', 'instrumentalness', 'liveness', 
+           'valence', 'tempo']
+
+print(df_clean.columns)
+print(df_clean['genre'].value_counts())
+
+X = df_clean[features]
+y = df_clean['genre']
+
+scaler = StandardScaler()
+scaler.fit(X)
+X_scaled = scaler.transform(X)
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, stratify=y, random_state=42)
+
+X_train.to_csv('X_train.csv')
+X_test.to_csv('X_test.csv')
+y_train.to_csv('y_train.csv')
+y_test.to_csv('y_test.csv')
+
 
