35980fda3c76b2ba9377788c45e046c81e7953d1
abirge12 <142052808+abirge12@users.noreply.github.com>
2025-06-13 15:47:04 -0400

    update svm

--- SVM.py
+++ SVM.py
@@ -1,32 +1,38 @@
-""" SVM """
+# """ SVM """
 
 from sklearn.svm import SVC
 import pandas as pd
-from preprocessing import transform_data, preprocess_data
+from preprocessing import transform_data, preprocess_data, get_data
 from sklearn.metrics import accuracy_score
+from sklearn.multioutput import MultiOutputClassifier
 
 
 
-df_clean = transform_data('songs.csv', 'genre')
+def main():
 
-features = ['duration_ms', 'popularity', 'danceability', 'energy', 'loudness', 
-           'speechiness', 'acousticness', 'instrumentalness', 'liveness', 
-           'valence', 'tempo']
+    X_train, X_test, y_train, y_test, songs_train, songs_test = get_data()  
 
-X = df_clean[features]
-y = df_clean['genre']
 
-X_train, X_test, y_train, y_test = preprocess_data(X, y)
+    df = transform_data('songs.csv', 'genre')
 
-model = SVC(
-    class_weight='balanced',
-    random_state=42,) # any other parameter necesssary?
+    genre_counts = df['genre'].value_counts()
+    valid_genres = genre_counts[genre_counts >= 2].index.tolist()
+    removed_genres = genre_counts[genre_counts < 2].tolist()
+    print(f'Removed genres: {removed_genres}')  
 
-model.fit(X_train, y_train)
+    model = MultiOutputClassifier(SVC(
+        class_weight='balanced',
+            kernel='rfr',
+            gamma='scale',    # account for unbalanced data
+        random_state=42,), n_jobs=-1) #n_jobs=-1 trains all SVMs in parallel
 
-y_pred = model.predict(X_test)
 
-accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
+    model.fit(X_train, y_train)
 
-#feature importance? confusion matrix?
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    print(accuracy)
+
+if __name__ == "__main__":
+    main()
