3f4bec42804bf347eb792458998cab4a0d2aafa7
abirge12 <142052808+abirge12@users.noreply.github.com>
2025-06-13 16:09:57 -0400

    update svm

--- SVM.py
+++ SVM.py
@@ -1,28 +1,50 @@
-# """ SVM """
+""" SVM """
 
 from sklearn.svm import SVC
 import pandas as pd
 from preprocessing import transform_data, preprocess_data, get_data
 from sklearn.metrics import accuracy_score
 from sklearn.multioutput import MultiOutputClassifier
+import numpy as np
 
+# ... should this be added to preprocessing ?
+def create_genre_vectors(songs_train, songs_test, df, genres):
+    
+    df_all_song_genres = df.groupby('song')['genre'].apply(set).to_dict()
+    
+    y_train_vector_list = []
+    for song in songs_train:
+        song_genres = df_all_song_genres.get(song, set())
+        true_genres = [1 if genre in song_genres else 0 for genre in genres]
+        y_train_vector_list.append(true_genres)
+    
+    y_test_vector_list = []
+    for song in songs_test:
+        song_genres = df_all_song_genres.get(song, set())
+        true_genres = [1 if genre in song_genres else 0 for genre in genres]
+        y_test_vector_list.append(true_genres)
+    
+    return np.array(y_train_vector_list), np.array(y_test_vector_list)
 
 
 def main():
 
-    X_train, X_test, y_train, y_test, songs_train, songs_test = get_data()  
-
-
     df = transform_data('songs.csv', 'genre')
 
-    genre_counts = df['genre'].value_counts()
-    valid_genres = genre_counts[genre_counts >= 2].index.tolist()
-    removed_genres = genre_counts[genre_counts < 2].tolist()
-    print(f'Removed genres: {removed_genres}')  
+    X_train, X_test, _, _, songs_train, songs_test = get_data()  
+
+    unique_genres = df['genre'].unique().tolist()
+
+    y_train, y_test = create_genre_vectors(
+        songs_train=songs_train,
+        songs_test=songs_test, 
+        df=df,
+        genres=unique_genres)
+
 
     model = MultiOutputClassifier(SVC(
         class_weight='balanced',
-            kernel='rfr',
+            kernel='rbf',
             gamma='scale',    # account for unbalanced data
         random_state=42,), n_jobs=-1) #n_jobs=-1 trains all SVMs in parallel
 
